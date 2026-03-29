62d4b0ee22f8f76eaa41c18e48f5911eb4c97919
00life <116957019+00life@users.noreply.github.com>
2026-03-28 23:31:02 -0400

    Update raspberrypi_remote.sh

--- raspberrypi_remote.sh
+++ raspberrypi_remote.sh
@@ -56,7 +56,7 @@ func_email() {
       curl -X POST
       -H "Content-Type: application/json" 
       -H "Accept: application/json"
-      -d "{\"Email\":\"pawn88@live.com\",\"Subject\":\"RASPBERRYPI_REMOTE_OUTPUT\", \"Message\": \"$1\"}"
+      -d "{\"Email\":\"pawn88@live.com\",\"Subject\":\"RASPBERRYPI_REMOTE_OUTPUT\", \"Message\": \"$MSG\"}"
       https://script.google.com/macros/s/AKfycbzzVxX1O0UTSzHBe7UElCNwnVPZrU3GqE98pmrivrQajqqM8QEe477O6MEl8gbhimozCg/exec
 EOF
 };
@@ -86,6 +86,6 @@ func_compare() {
 };
 
 echo
-echo test17
+echo test18
 #/ [ Run Main Function ]
 func_compare
